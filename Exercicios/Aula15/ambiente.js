dc133881cb9aaffcf899508146355c4ca3f2511d
Tiago Alves <140011451+TiagoAlvesbr01@users.noreply.github.com>
2023-08-18 17:28:54 -0300

    js

---
+++ Exercicios/Aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,2,9,3]
+num[3] = 6
+num.sort()
+
+num.push(7)
+console.log(num)
+
+
+console.log(`O vetor tem ${num.length} posicoes`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
